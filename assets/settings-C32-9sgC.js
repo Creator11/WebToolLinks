046ee25e944e00c61cfe0405c3443dce2e3123ec
Creator11 <trohin.danil2015@yandex.ru>
2025-02-23 17:52:08 +0400

    Updates

--- assets/settings-C32-9sgC.js
+++
@@ -1,1 +0,0 @@
-import{u as t}from"./index-uoPfs4lm.js";const s={},n=()=>t("div",{className:s.settings,children:"settings"});export{n as default};
---
+++ assets/settings-COcEuGTJ.js
@@ -0,0 +1,1 @@
+import{u as t}from"./index-CVFr-BV0.js";const s={},n=()=>t("div",{className:s.settings,children:"settings"});export{n as default};
